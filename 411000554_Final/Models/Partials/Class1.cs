f6137fec80b2196c99f07a02e68b4574ae73af6d
xiaojie4082 <xiaojie4082@gmail.com>
2023-06-07 20:15:13 +0800

    ADD DataAnnotations

---
+++ 411000554_Final/Models/Partials/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _411000554_Final.Models
+{
+    [MetadataType(typeof(TableMetadata))]
+    public partial class Table
+    {
+     
+    }
+
+    public class TableMetadata
+    {
+        public int Id
+        {
+            get; set;
+        }
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "標題")]
+        [StringLength(50,ErrorMessage ="請勿超過50個字元")]
+        public string Title
+        {
+            get; set;
+        }
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "內容")]
+        [StringLength(500)]
+        public string Content
+        {
+            get; set;
+        }
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "發布日期")]
+        public Nullable<System.DateTime> PublishDate
+        {
+            get; set;
+        }
+    }
+}
