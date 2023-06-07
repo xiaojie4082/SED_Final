4a7f2cd180ec807f6423bb49ed43e96d1e015994
xiaojie4082 <xiaojie4082@gmail.com>
2023-06-07 18:55:07 +0800

    ADD /Test/Eaxm

---
+++ 411000554_Final/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _411000554_Final.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Eaxm()
+        {
+            return View();
+        }
+    }
+}
